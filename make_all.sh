661ee8d313c8574abdf97054c9d9650320bd325f
Stuart Woodward <stuart@87zero.com>
2024-02-29 14:49:03 +0900

    Move site to docs

--- make_all.sh
+++ make_all.sh
@@ -1,3 +1,4 @@
-./jpred.py site/j1.html columns/j1.cols
-./jpred.py site/j2.html columns/j2.cols
-./jpred.py site/j3.html columns/j3.cols
+mkdir -p docs
+./jpred.py docs/j1.html columns/j1.cols
+./jpred.py docs/j2.html columns/j2.cols
+./jpred.py docs/j3.html columns/j3.cols
