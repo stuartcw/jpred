f2000a5d9ec0c1ac15bec3d6128cda8f4d83d134
Stuart Woodward <stuart@87zero.com>
2024-03-02 16:41:46 +0900

    Delete the created files and make the folders. Make the user prediction pages.

--- make_all.sh
+++ make_all.sh
@@ -1,4 +1,11 @@
 mkdir -p docs
+mkdir -p docs/preds
+rm jpred.db
+./import.py
+rm -r docs/j*.html
+rm docs/users.html
+rm -r docs/preds/*.html
 ./jpred.py docs/j1.html columns/j1.cols
 ./jpred.py docs/j2.html columns/j2.cols
 ./jpred.py docs/j3.html columns/j3.cols
+./jpred_users.py
