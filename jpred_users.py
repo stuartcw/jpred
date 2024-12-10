f67af6536deda4f96c8b16142970a3a150877f05
Stuart Woodward <stuart@87zero.com>
2024-12-10 21:06:20 +0900

    Write the engine to calculate the scores.

--- jpred_users.py
+++ jpred_users.py
@@ -1,10 +1,14 @@
-#!venv/bin/python3.12
+#!/usr/bin/env python3
 import click
 import sys
 import sqlite3
 from jinja2 import Environment, FileSystemLoader
 
 from icecream import ic
+from collections import defaultdict
+
+# Create a nested defaultdict
+leaderboard = defaultdict(lambda: defaultdict(int))
 
 # Function to connect to the SQLite database
 def create_connection(db_file):
@@ -16,90 +20,156 @@ def create_connection(db_file):
         print(e)
     return conn
 
-# Function to perform the query and return results
-def query_database(conn, name):
-    cursor = conn.cursor()
-    cursor.execute(f'''SELECT
-  Timestamp,
-  Name,
-  "J1 First Place",
-  "J1 Second Place",
-  "J1 Third Place",
-  "J1 Third from Last Place",
-  "J1 Second from Last Place",
-  "J1 Last Place",
-  "J2 First Place",
-  "J2 Second Place",
-  "J2 Third Place",
-  "J2 FORTH Place" as "J2 Forth Place",
-  "J2 FIFTH Place" as "J2 Fifth Place",
-  "J2 SIXTH Place" as "J2 Sixth Place",
-  "J2 Third from Last Place",
-  "J2 Second from Last Place",
-  "J2 Last Place",
-  "J3 First Place",
-  "J3 Second Place",
-  "J3 Third Place",
-  "J3 FORTH Place" as "J3 Forth Place",
-  "J3 FIFTH Place" as "J3 Fifth Place",
-  "J3 SIXTH Place" as "J3 Sixth Place",
-  "J3 Third from Last Place",
-  "J3 Second from Last Place",
-  "J3 Last Place"
-FROM jpred
-    WHERE Name LIKE "{name}"''')
-    return cursor 
-
-def write_one_user(conn,name,html_filename,env):
-    ic(name)
-    leagues = ["j1","j2","j3"]
-    league_columns = []
-
-    predictions={}
-    for league in leagues:
-        columns_file="columns/"+league+".cols"
-        with open(columns_file, 'r') as file:
-              columns: list[str]  = file.read().splitlines() 
-              league_columns.append((league,columns))
-
-    # Load Jinja environment and template
-    env = Environment(loader=FileSystemLoader('.'))
-    template = env.get_template('templates/user_template.html')
-
-    if conn is not None:
-       
-        with open(html_filename,"w") as html_file:
-            cursor = query_database(conn, name)
-            while row := cursor.fetchone() :
-                row_dict = {key: row[key] for key in row.keys()}
-                
-                for league,columns in league_columns:
-                    table_row=[]
-                    #ic(league,columns)
-                    for column in columns:
-                        #j1 = { key:value for key, value in row_dict.items() if key.startswith("J1") }
-                        #ic(column,row[column])
-                        table_row.append((column.title(),row[column]))
-                    #ic(30*"-")
-                    predictions[league]=table_row
-
-            #ic(predictions.keys())
-            #ic(predictions["j1"])
-            #ic(predictions["j2"])
-
-            # Render the HTML content using the template
-            html_content = template.render(predictions=predictions, name=name, league=league)
-            html_file.write(html_content)
-
-            print(f'HTML file {html_filename} has been created with the query results.')
-    else:
-        print('Error! Cannot connect to the database.')
 
 def get_all_users(conn):
     cursor = conn.cursor()
     cursor.execute(f'''SELECT Name FROM jpred''')
     # return as a list strings
     return [row["Name"] for row in cursor.fetchall()]
+
+league_predictions= { 
+    "j1": [
+        "J1 First Place",
+        "J1 Second Place",
+        "J1 Third Place",
+        "J1 Third from Last Place",
+        "J1 Second from Last Place",
+        "J1 Last Place"
+    ],
+    "j2": [
+        "J2 First Place",
+        "J2 Second Place",
+        "J2 Third Place",
+        "J2 FORTH Place",
+        "J2 FIFTH Place",
+        "J2 SIXTH Place",
+        "J2 Third from Last Place",
+        "J2 Second from Last Place",
+        "J2 Last Place"
+     ],
+    "j3": [
+        "J3 First Place",
+        "J3 Second Place",
+        "J3 Third Place",
+        "J3 FORTH Place",
+        "J3 FIFTH Place",
+        "J3 SIXTH Place",
+        "J3 Third from Last Place",
+        "J3 Second from Last Place",
+        "J3 Last Place"
+    ]
+}
+
+def j1_score(index):
+    if index < 4:
+        return f"""CASE
+          WHEN position = {index} THEN 1 ELSE 0 END
+          + CASE WHEN position IN (1, 2, 3) THEN 1 ELSE 0
+            END"""
+    else:
+        return f"""CASE
+          WHEN position = {index+11} THEN 1 ELSE 0 END
+          + CASE WHEN position IN (18, 19, 20 ) THEN 1 ELSE 0
+            END"""
+
+def j2_j3_score(index):
+    if index < 7:
+        return f"""CASE
+          WHEN position = {index} THEN 1 ELSE 0 END
+          + CASE WHEN position IN (1, 2, 3, 4, 5, 6) THEN 1 ELSE 0
+            END"""
+    else:
+        return f"""CASE
+          WHEN position = {index+11} THEN 1 ELSE 0 END
+          + CASE WHEN position IN (18, 19, 20 ) THEN 1 ELSE 0
+            END"""
+
+
+score_criteria={ 
+                "j1": j1_score,
+                "j2": j2_j3_score,
+                "j3": j2_j3_score
+        }
+
+
+def make_league_score_SQL(name,league,year):
+    table=f"{league}_{year}"
+    sql_unions=[]
+
+    for index, prediction_name in enumerate(league_predictions[league],start=1):
+        #nic(index, prediction_name)
+
+        ONE_PREDICTION_SQL=f"""
+        SELECT {index} as "Index", Position, Prediction, Team, Position, 
+            {score_criteria[league](index)} Score FROM
+            (
+            SELECT *, (SELECT Position FROM {table} WHERE {table}.Team = prediction.Team) AS Position 
+            FROM (SELECT 
+                    Name, 
+                    [Email address] as Email, 
+                    {index} AS "Order", 
+                    '{prediction_name.title()}' as Prediction , 
+                    [{prediction_name}] AS Team
+                FROM jpred
+                WHERE Name = '{name}'
+            ) as prediction )"""
+
+        sql_unions.append(ONE_PREDICTION_SQL)
+
+
+    #ic(sql_unions)
+    return " UNION ".join(sql_unions)
+
+def query_database(conn, sql):
+    cursor = conn.cursor()
+    cursor.execute(sql)
+    return cursor
+
+
+
+def write_one_user(conn,name,html_filename,env,year=2024):
+    ic(name)
+    leagues =  ["j1","j2","j3"]
+    total_score= 0
+    total_exact_matches=0
+
+    predictions= {}
+
+    # Load Jinja environment and template
+    env = Environment(loader=FileSystemLoader('.'))
+    template = env.get_template('templates/user_template.html')
+
+    if conn is None:
+        print('Error! Cannot connect to the database.')
+        sys.exit()
+    else:
+        with open(html_filename,"w") as html_file:
+            for league in leagues:
+                predictions[league]=[]
+                sql=make_league_score_SQL(name,league,year)
+                cursor = query_database(conn, sql)
+
+                while row := cursor.fetchone() :
+                    row_dict = {key: row[key] for key in row.keys()}
+                    # ic(row_dict)
+                    predictions[league].append(row_dict)
+
+                #ic(predictions)
+                scores=[ int(row["Score"]) for row  in predictions[league]]
+                score_for_this_league =sum(scores)
+                exact_matches= scores.count(2)
+                leaderboard[name][league]=score_for_this_league
+                total_score += score_for_this_league
+                total_exact_matches += exact_matches
+
+            leaderboard[name]["total_score"]=total_score
+            leaderboard[name]["total_exact_matches"]=total_exact_matches
+            html_content = template.render(predictions=predictions, name=name, league=league, total_score=total_score, year=year)
+            html_file.write(html_content)
+        print(f'HTML file {html_filename} has been created with the query results.')
+
+def _main():
+    print(make_league_score_SQL("Stuart Woodward","j3","2024"))
 
 def main():
     db_path = 'jpred.db'
@@ -108,7 +178,7 @@ def main():
     
     conn = create_connection(db_path)
     names=get_all_users(conn)
-    for name in names:
+    for name in names: # $$
         # ic(name)
         html_filename = "docs/preds/"+name+".html"
         if name.find("/") > 0:
@@ -116,12 +186,24 @@ def main():
             continue
         write_one_user(conn,name,html_filename,env)
     # Render the HTML content using the template
+
+    ordered_leaderboard=[]
+
+    def sort_key(item):
+        return (item[2]["total_score"] , item[2]["total_exact_matches"],item[2]["j1"],item[2]["j2"],item[2]["j3"])
+
+
+    for name in leaderboard:
+        ordered_leaderboard.append((leaderboard[name]["total_score"],name,leaderboard[name]))
+    ordered_leaderboard=sorted(ordered_leaderboard, reverse=True, key=sort_key)
     
     template = env.get_template('templates/users.html')
-    html_content = template.render(names=names)
+    html_content = template.render(ordered_leaderboard=ordered_leaderboard)
     html_filename="docs/users.html"
     with open(html_filename,"w") as html_file:
         html_file.write(html_content)
+
+    ic(ordered_leaderboard)
     
 
 if __name__ == '__main__':
