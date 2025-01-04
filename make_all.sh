fd71a979cb61479dca236c092c4526bfd2ed4dc1
Stuart Woodward <stuart@87zero.com>
2025-01-04 20:39:17 +0900

    Actually make everything. Set up the venv environment automatically. Copy over the favicons.

--- make_all.sh
+++ make_all.sh
@@ -1,11 +1,15 @@
+source bin/activate
 mkdir -p docs
 mkdir -p docs/preds
 rm jpred.db
-./import.py
 rm -r docs/j*.html
 rm docs/users.html
 rm -r docs/preds/*.html
+./create_db.sh
 ./jpred.py docs/j1.html columns/j1.cols
 ./jpred.py docs/j2.html columns/j2.cols
 ./jpred.py docs/j3.html columns/j3.cols
 ./jpred_users.py
+cp assets/favicons/*.png docs
+cp assets/favicons/*.ico docs
+cp assets/favicons/site.webmanifest docs
