28b2146e4c47100cb0afc61ad4d3db9d37e6fc23
Stuart Woodward <stuart@87zero.com>
2025-12-08 08:57:04 +0900

    - Changed the shebang to use `uv` for running the script. - Added metadata comments specifying required Python version and dependencies (Click and Jinja2).

--- jpred.py
+++ jpred.py
@@ -1,4 +1,11 @@
-#!/usr/bin/env python3
+#!/usr/bin/env -S uv run --script
+# /// script
+# requires-python = ">=3.11"
+# dependencies = [
+#     "click",
+#     "jinja2",
+# ]
+# ///
 import click
 import sqlite3
 from jinja2 import Environment, FileSystemLoader
